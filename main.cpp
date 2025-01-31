309616019bfbbb0edc29a37acdab21be9093af1d
mohamed1ahmed2galal3 <mohamedahmedgalal246@gmail.com>
2025-01-31 18:35:35 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int mult(int x, int y)
+{
+    return x* y;
+}
+int main()
+{
+
+    cout << "Hello world!" << endl;
+    cout << "5 * 2 = "<<mult(2,5)  << endl;
+    cout<<"Mohamed";
+    return 0;
+}
